01851ea970cfbee1d9b1d1a830386a6f9e5490ba
Jonathan <jonathan.llopis.linares@gmail.com>
2024-12-10 20:30:17 +0100

    Primer Commit

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Real Estate",
+    "version": "0.2",
+    "category": "Sales/Real Estate",
+    "application": True,
+    "data": ["security/ir.model.access.csv", "views/estate_property_views.xml", "views/estate_menus.xml"],
+}
